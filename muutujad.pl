07b2cc649fa5e7826061ceaa45176300fbae3688
Indrek Hein <webmaster@eki.ee>
2014-12-15 17:58:20 +0200

    Create muutujad.pl
    Ühine skriptide komplektile

---
+++ muutujad.pl
@@ -0,0 +1,17 @@
+
+our $home_dir = '/home/indrek';
+our $in_dir = $home_dir . '/in';
+our $out_dir = $home_dir . '/out';
+our $bin_dir = $home_dir . '/tarkvara';
+
+our $g_haalenimi = 'naine';
+#our $g_haalenimi = 'tonu_hts';
+
+our $g_bitte = 16;	# bits per sample
+our $g_sagedus = 48000;	# sampling rate
+our $g_kanaleid = 1;	# 1 mono
+
+our $g_lausepaus = 0.4;	# 400 ms lausete vahel ( . ? ! )
+our $g_pikempaus = 0.8;	# 800 ms mõttepunktid, kõnelejavahetus
+our $g_loigupaus = 1.0;	# 1000 ms lõikude vahel
+
